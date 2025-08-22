f53395064d6b1391b2ee85f948ccb2ccdb1fcb47
PapaJoeu <116471554+PapaJoeu@users.noreply.github.com>
2025-08-21 21:45:31 -0600

    fix: update sheet size options with names and add missing entries

--- sizeOptions.js
+++ sizeOptions.js
@@ -6,25 +6,33 @@ export const SIZE_OPTIONS = {
         { width: 12, length: 18, name: "Tabloid Extra" },
         { width: 13, length: 19, name: "Super B" },
         { width: 8.5, length: 11, name: "Letter" },
-        { width: 9, length: 12 },
-        { width: 10, length: 13 },
-        { width: 11.5, length: 17.5, name: "Mike's Hemp Paper" },
+        { width: 9, length: 12, name: "Letter Plus" },
+        { width: 10, length: 13, name: "Letter Plus Plus"},
+        { width: 11.5, length: 17.5, name: "Hemp Paper" },
         { width: 11, length: 17, name: "Tabloid" },
         { width: 14.66, length: 25, name: "Awful Oversize" }
     ],
     doc: [
         { width: 3.5, length: 4, name: "Folded Business Card" },
-        { width: 4.25, length: 11, name: "Door Hanger" }
+        { width: 4.25, length: 11, name: "Door Hanger" },
+        { width: 4.25, length: 5.5, name: "Quarter Letter" },
+        { width: 5, length: 7, name: "5x7" },
+        { width: 5.5, length: 8.5, name: "Half Letter" },
+        { width: 6, length: 9, name: "Sunrun Postcard" },
+        { width: 4, length: 6, name: "Chipotle" },
     ],
     gutter: [
         { width: 0.125, length: 0.125, name: "1/8" },
         { width: 0.25, length: 0.25, name: "1/4" },
         { width: 0, length: 0 },
         { width: 0.3125, length: 0.17, name: "Duplo 25 UP" }
+
     ],
     margin: [
-        { width: 0.25, length: 0.25 },
-        { width: 0.5, length: 0.5 },
-        { width: 0, length: 0 }
+        { width: 0.0625, length: 0.0625, name: "1/16" },
+        { width: 0.25, length: 0.25, name: "1/4" },
+        { width: 0.5, length: 0.5, name: "1/2" },
+        { width: 0, length: 0, name: "No Margin" },
+        { width: 0.125, length: 0.125, name: "1/8" },
     ]
 };
